c4f58495220b80a983ac633f6e613eae5156df52
Olurotimi Olugbemi <56795678+RotimiCodes@users.noreply.github.com>
2024-07-15 03:36:20 +0100

    Rotimi Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/")
